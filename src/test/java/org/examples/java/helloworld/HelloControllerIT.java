18602344896dcea2daeba10c4bce41b5e83f88a4
Arun Gupta <arun.gupta@gmail.com>
2018-03-23 15:17:14 -0700

    updated the test

--- src/test/java/org/examples/java/helloworld/HelloControllerIT.java
+++ src/test/java/org/examples/java/helloworld/HelloControllerIT.java
@@ -36,6 +36,6 @@ public class HelloControllerIT {
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+        assertThat(response.getBody(), equalTo("Hello Jenkins X + Spring Boot!"));
     }
 }
