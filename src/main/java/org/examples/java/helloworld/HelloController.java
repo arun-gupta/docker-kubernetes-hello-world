ee26a07607b747cd527965884a916c86a470db0b
Arun Gupta <arun.gupta@gmail.com>
2018-03-23 15:13:52 -0700

    updating the message

--- src/main/java/org/examples/java/helloworld/HelloController.java
+++ src/main/java/org/examples/java/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Hello Jenkins X + Spring Boot!";
     }
     
 }
